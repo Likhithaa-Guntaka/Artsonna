c7e57b42bbbaf6750ffb9519d61f6228368b1d3c
Aimee Jiang <xj2457@nyu.edu>
2026-08-15 18:40:52 +0000

    File changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -40,6 +40,8 @@ module.exports = {
   				DEFAULT: 'hsl(var(--destructive))',
   				foreground: 'hsl(var(--destructive-foreground))'
   			},
+  			home: 'hsl(var(--home-background))',
+  			terracotta: 'hsl(var(--terracotta))',
   			border: 'hsl(var(--border))',
   			input: 'hsl(var(--input))',
   			ring: 'hsl(var(--ring))',
